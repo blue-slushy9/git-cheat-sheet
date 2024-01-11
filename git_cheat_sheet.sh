698bb65c96f74bda1dca51dd2df2c64c2d05a7ea
R <107578323+blue-slushy9@users.noreply.github.com>
2024-01-11 09:20:14 -0800

    Update git_cheat_sheet.sh

--- git_cheat_sheet.sh
+++ git_cheat_sheet.sh
@@ -1,22 +1,5 @@
-# This is a Git cheat-sheet I am putting together...
-
-# Create new GitHub repo from Linux terminal;
-# Replace 'YOUR_GITHUB_USERNAME' with your GitHub username;
-# Replace 'YOUR_ACCESS_TOKEN' with your GitHub personal access token;
-# Replace 'NEW_REPO_NAME' with the name of your new repository;
-curl -u "YOUR_GITHUB_USERNAME:YOUR_ACCESS_TOKEN" https://api.github.com/user/repos -d '{"name":"NEW_REPO_NAME"}'
-
-############
-
-# Got this command from GitHub, it is used to create a repo from the terminal,
-# but I couldn't get it to work yet;
-echo "# <package-name>" >> README.md
-git init
-git add README.md
-git commit -m "First commit"
-git branch -M main
-git remote add origin https://github.com/blue-slushy9/<repo-name>.git
-git push -u origin main
+# View status of current local Git repo, i.e. is it up to date with main branch?;
+git status
 
 # Verify your username in Git config;
 git config user.name
@@ -29,3 +12,23 @@ git config --global user.name "<username>"
 
 # Set global email, quotations are necessary;
 git config --global user.email "<email address>"
+
+########################## NONESSENTIAL
+
+# This is a Git cheat-sheet I am putting together...
+
+# Create new GitHub repo from Linux terminal;
+# Replace 'YOUR_GITHUB_USERNAME' with your GitHub username;
+# Replace 'YOUR_ACCESS_TOKEN' with your GitHub personal access token;
+# Replace 'NEW_REPO_NAME' with the name of your new repository;
+curl -u "YOUR_GITHUB_USERNAME:YOUR_ACCESS_TOKEN" https://api.github.com/user/repos -d '{"name":"NEW_REPO_NAME"}'
+
+# Got this command from GitHub, it is used to create a repo from the terminal,
+# but I couldn't get it to work yet;
+echo "# <package-name>" >> README.md
+git init
+git add README.md
+git commit -m "First commit"
+git branch -M main
+git remote add origin https://github.com/blue-slushy9/<repo-name>.git
+git push -u origin main
