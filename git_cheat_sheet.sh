512f27d8c8a1a3293ac803b8bb4438876a885160
blue-slushy9 <ra.cruz971@gmail.com>
2024-02-13 13:58:54 -0800

    added a few git commands i've been wondering about

--- git_cheat_sheet.sh
+++ git_cheat_sheet.sh
@@ -1,5 +1,18 @@
 # This is a Git cheat-sheet I am putting together...
 
+# Retrieve latest changes from remote repo without integrating those changes
+# into the local repo;
+git fetch
+
+# Retrieve latest changes from remote repo AND integrate those changes into
+# local repo;
+git pull
+
+# Display information about the current state of the local repository, 
+# e.g. modified files, staged changes, and untracked files;
+git status
+
+
 # Got this command from GitHub, it is used to create a repo from the terminal,
 # but I couldn't get it to work yet;
 echo "# <package-name>" >> README.md
